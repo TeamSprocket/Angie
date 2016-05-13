ed9b546e62ee937638dc34f2d969864cb8da8e34
Forest Hu <foresthu2006@gmail.com>
2016-05-12 19:57:52 -0700

    fixed spacing

--- src/org/usfirst/frc/team3473/robot/Phannels.java
+++ src/org/usfirst/frc/team3473/robot/Phannels.java
@@ -12,21 +12,22 @@ public interface Phannels {
 		 */
     	
 		PORT_hangActuation = 0,
-    	PORT_drivebaseBackRIGHT = 1,
-    	PORT_drivebaseCenterRIGHT = 2,
-    	PORT_drivebaseFrontRIGHT = 3,
-    	PORT_spool = 4,
-    	PORT_drivebaseBackLEFT = 5,
-    	PORT_drivebaseCenterLEFT = 6,
-    	PORT_drivebaseFrontLEFT = 7,
-    	PORT_intakeActuation = 8,
-    	PORT_intakeTop = 9,
-    	PORT_intakeBot = 10,
-    	PORT_hangExtension = 11,
+    		PORT_drivebaseBackRIGHT = 1,
+    		PORT_drivebaseCenterRIGHT = 2,
+    		PORT_drivebaseFrontRIGHT = 3,
+    		PORT_spool = 4,
+    		PORT_drivebaseBackLEFT = 5,
+    		PORT_drivebaseCenterLEFT = 6,
+    		PORT_drivebaseFrontLEFT = 7,
+    		PORT_intakeActuation = 8,
+    		PORT_intakeTop = 9,
+    		PORT_intakeBot = 10,
+    		PORT_hangExtension = 11,
 	
 		/**
 		 * Analog IO
 		 */
-    	PORT_gyro = 0;
+		 
+    		PORT_gyro = 0;
     	
 }
